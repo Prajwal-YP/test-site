deed2af1940d83f9e363602740aa1d7fc4317e63
Prajwal-YP <ypprajsanjuyp@gmail.com>
2024-07-17 23:50:07 +0530

    formated spaces

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,19 @@
+// Submit button
 document.getElementById("Submit").addEventListener("click", function () {
     const username = document.getElementById("Name").value;
     const password = document.getElementById("Pass").value;
-      
+
 
     if (username === "YP" && password === "123") {
-      alert("Login successful!");
+        alert("Login successful!");
     } else {
-      alert("Login failed. Please check your credentials.");
+        alert("Login failed. Please check your credentials.");
     }
-  });
+});
 
-  document.getElementById("Reset").addEventListener("click", function () {
+
+//   Reset button``
+document.getElementById("Reset").addEventListener("click", function () {
     document.getElementById("Name").value='';
     document.getElementById("Pass").value='';
-  });
+});

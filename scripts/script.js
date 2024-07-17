a421f1bbfa8b07a66eff729f033708c2ba83f7eb
Prajwal-YP <ypprajsanjuyp@gmail.com>
2024-07-17 23:45:30 +0530

    script logic transfered to scripts.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
+document.getElementById("Submit").addEventListener("click", function () {
+    const username = document.getElementById("Name").value;
+    const password = document.getElementById("Pass").value;
+      
 
+    if (username === "  " && password === "123") {
+      alert("Login successful!");
+    } else {
+      alert("Login failed. Please check your credentials.");
+    }
+  });
